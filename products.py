db95f2da1d2a412e8e5d34bba9550e1add3929af
chen xiao <1090021294@qq.com>
2019-08-30 13:39:05 +0800

    add read file

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
+# 读取档案
 products = []
+with open ('products.csv', 'r' ,encoding = 'utf-8') as f:
+	for line in f:
+		if '商品,价格' in line:
+			continue # 继续，跳到下一回，下面的不执行
+		s = line.strip().split(',')
+		products.append(s)
+print(products)
+
+ # 让使用者输入
 while True:
 	name = input('请输入商品名称： ')	
 	if name == 'q':
@@ -8,14 +18,15 @@ while True:
 	# product = []
 	# product.append(name)
 	# product.append(price)
-
 	# product = [name, price]
 	products.append([name, price])
 print(products)
 
+# 印出所有购买记录
 for p in products:
 	print(p[0], '的价格是', p[1])
 
+ #写入档案
 with open('products.csv', 'w', encoding = 'utf-8') as f:
 	f.write('商品' + ',' + '价格' + '\n')
 	for p in products:	
