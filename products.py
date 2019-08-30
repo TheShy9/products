5e1db72ad99132f809049c6ac5dea973e3de58da
chen xiao <1090021294@qq.com>
2019-08-30 13:52:23 +0800

    add find file:

--- products.py
+++ products.py
@@ -1,14 +1,22 @@
-# 读取档案
-products = []
-with open ('products.csv', 'r' ,encoding = 'utf-8') as f:
-	for line in f:
-		if '商品,价格' in line:
-			continue # 继续，跳到下一回，下面的不执行
-		s = line.strip().split(',')
-		products.append(s)
-print(products)
+# 检查档案在不在
+import os # orerating system
 
- # 让使用者输入
+products = []
+if os.path.isfile('products.csv'):
+	print('nice,找到档案了')
+# 读取档案
+	with open ('products.csv', 'r' ,encoding = 'utf-8') as f:
+		for line in f:
+			if '商品,价格' in line:
+				continue # 继续，跳到下一回，下面的不执行
+			name,price = line.strip().split(',')
+			products.append([name,price])
+	print(products)	
+else:
+	print('找不到档案')
+
+
+# 让使用者输入
 while True:
 	name = input('请输入商品名称： ')	
 	if name == 'q':
@@ -26,7 +34,7 @@ print(products)
 for p in products:
 	print(p[0], '的价格是', p[1])
 
- #写入档案
+# 写入档案
 with open('products.csv', 'w', encoding = 'utf-8') as f:
 	f.write('商品' + ',' + '价格' + '\n')
 	for p in products:	
